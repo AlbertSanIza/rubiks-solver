03e3338c055248bf0ba8b5b7931d12cb07f13a1c
Albert Sanchez <albert.san.iza@gmail.com>
2017-06-30 14:49:31 -0700

    added gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var tinylr = require('tiny-lr');
+
+gulp.task('watch', () => {
+  var lr = tinylr();
+  lr.listen(35729, () => {
+    console.log('... LiveReload on localhost:35729\n');
+  });
+  gulp.watch(['index.html', 'js/**/*', 'css/**/*'], evt => {
+    console.log(evt.path);
+    lr.changed({
+      body: {
+        files: [evt.path]
+      }
+    });
+  });
+});
