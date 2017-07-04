712b869837cd2ae9c1ffd888c76b0cda5a5e312f
Albert Sanchez <albert.san.iza@gmail.com>
2017-07-04 10:31:32 -0700

    removed ;

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-var gulp = require('gulp');
-var tinylr = require('tiny-lr');
+var gulp = require('gulp')
+var tinylr = require('tiny-lr')
 
 gulp.task('watch', () => {
-  var lr = tinylr();
+  var lr = tinylr()
   lr.listen(35729, () => {
-    console.log('... LiveReload on localhost:35729\n');
-  });
+    console.log('... LiveReload on localhost:35729\n')
+  })
   gulp.watch(['index.html', 'js/**/*', 'css/**/*'], evt => {
-    console.log(evt.path);
+    console.log(evt.path)
     lr.changed({
       body: {
         files: [evt.path]
       }
-    });
-  });
-});
+    })
+  })
+})
